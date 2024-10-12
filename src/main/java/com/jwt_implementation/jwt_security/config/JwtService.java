9706e3c4a59137ac05012d2ca172c62bd8d68949
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-12 13:58:37 +0300

    Create the JWT service

---
+++ src/main/java/com/jwt_implementation/jwt_security/config/JwtService.java
@@ -0,0 +1,11 @@
+package com.jwt_implementation.jwt_security.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String token) {
+        return null;
+    }
+}
