f59352f5cd1d1a33e4e68dc5ba3793cf39187215
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-12 14:53:56 +0300

    Implement the getSignInKey method & Extract a single claim from JWT

--- src/main/java/com/jwt_implementation/jwt_security/config/JwtService.java
+++ src/main/java/com/jwt_implementation/jwt_security/config/JwtService.java
@@ -2,15 +2,30 @@ package com.jwt_implementation.jwt_security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "";
+    @Value("${JWT_SECRET_KEY}")
+    private String JWT_SECRET_KEY;
 
     public String extractUsername(String token) {
         return null;
+    }
+
+    public <T> T extractClaim(
+            String token,
+            Function<Claims, T> claimsResolver
+    ) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token) {
@@ -20,5 +35,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(JWT_SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
