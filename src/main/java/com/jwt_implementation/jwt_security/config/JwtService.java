8231822d4562fd633f9518cf712b8f242fb986f3
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-12 14:11:59 +0300

    Extract claims from JWT

--- src/main/java/com/jwt_implementation/jwt_security/config/JwtService.java
+++ src/main/java/com/jwt_implementation/jwt_security/config/JwtService.java
@@ -1,11 +1,24 @@
 package com.jwt_implementation.jwt_security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "";
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
